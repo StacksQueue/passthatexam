fb5d3012dc1ff152a409fbb096293dd1e9e1e24f
stacksqueue <paulelyson.villaceran@gmail.com>
2023-07-28 15:16:57 +0800

    fix header

--- src/app/components/score/score.component.ts
+++ src/app/components/score/score.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { Router } from '@angular/router';
 import { ExamService } from 'src/app/services/exam.service';
 import { ExamFilterComponent } from '../exam-filter/exam-filter.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 export interface Score {
   score: number;
@@ -14,17 +15,19 @@ export interface Score {
   styleUrls: ['./score.component.scss'],
 })
 export class ScoreComponent implements OnInit {
-  @Input() score: Score;
+  // @Input() score: Score;
 
   constructor(
     private router: Router,
     private examService: ExamService,
-    private _bottomSheet: MatBottomSheet
+    private _bottomSheet: MatBottomSheet,
+    public dialogRef: MatDialogRef<ScoreComponent>,
+    @Inject(MAT_DIALOG_DATA) public score: Score
   ) {}
 
   ngOnInit(): void {}
 
   openBottomSheet(): void {
-    this._bottomSheet.open(ExamFilterComponent);
+    // this._bottomSheet.open(ExamFilterComponent);
   }
 }
