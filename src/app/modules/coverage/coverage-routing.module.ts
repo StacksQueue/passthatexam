228ed8e2e08b129e23f9cbd5d1cb490cd073bfe6
stacksqueue <paulelyson.villaceran@gmail.com>
2023-09-28 19:21:43 +0800

    physical-science

--- src/app/modules/coverage/coverage-routing.module.ts
+++ src/app/modules/coverage/coverage-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoverageBiologicalScienceComponent } from 'src/app/components/coverage-biological-science/coverage-biological-science.component';
 import { CoverageEnglishComponent } from 'src/app/components/coverage-english/coverage-english.component';
 import { CoverageFilipinoComponent } from 'src/app/components/coverage-filipino/coverage-filipino.component';
+import { CoveragePhysicalScienceComponent } from 'src/app/components/coverage-physical-science/coverage-physical-science.component';
 
 const routes: Routes = [
   { path: 'english', component: CoverageEnglishComponent },
   { path: 'filipino', component: CoverageFilipinoComponent },
   { path: 'biological-science', component: CoverageBiologicalScienceComponent },
+  { path: 'physical-science', component: CoveragePhysicalScienceComponent },
 ];
 
 @NgModule({
--- src/app/modules/coverage/coverage.module.ts
+++ src/app/modules/coverage/coverage.module.ts
@@ -5,12 +5,14 @@ import { CoverageRoutingModule } from './coverage-routing.module';
 import { CoverageBiologicalScienceComponent } from 'src/app/components/coverage-biological-science/coverage-biological-science.component';
 import { CoverageEnglishComponent } from 'src/app/components/coverage-english/coverage-english.component';
 import { CoverageFilipinoComponent } from 'src/app/components/coverage-filipino/coverage-filipino.component';
+import { CoveragePhysicalScienceComponent } from '../../components/coverage-physical-science/coverage-physical-science.component';
 
 @NgModule({
   declarations: [
     CoverageBiologicalScienceComponent,
     CoverageEnglishComponent,
     CoverageFilipinoComponent,
+    CoveragePhysicalScienceComponent,
   ],
   imports: [CommonModule, CoverageRoutingModule],
 })
