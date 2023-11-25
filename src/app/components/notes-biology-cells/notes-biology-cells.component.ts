b79fa649d1b7f5d6bf3b4493b97a506d3e8be0b5
stacksqueue <paulelyson.villaceran@gmail.com>
2023-11-25 15:36:21 +0800

    biology - cells

---
+++ src/app/components/notes-biology-cells/notes-biology-cells.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-biology-cells',
+  templateUrl: './notes-biology-cells.component.html',
+  styleUrls: ['./notes-biology-cells.component.scss']
+})
+export class NotesBiologyCellsComponent {
+
+}
--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { NotesRoutingModule } from './notes-routing.module';
 import { NotesRizalComponent } from '../../components/notes-rizal/notes-rizal.component';
 import { NotesScienceEarthSpaceAstronomyComponent } from 'src/app/components/notes-science-earth-space-astronomy/notes-science-earth-space-astronomy.component';
+import { NotesBiologyCellsComponent } from 'src/app/components/notes-biology-cells/notes-biology-cells.component';
 
 @NgModule({
-  declarations: [NotesRizalComponent, NotesScienceEarthSpaceAstronomyComponent],
+  declarations: [NotesRizalComponent, NotesScienceEarthSpaceAstronomyComponent, NotesBiologyCellsComponent],
   imports: [CommonModule, NotesRoutingModule],
 })
 export class NotesModule {}
