b4e7bc04ecc33cf1f3f37a15afe86bb401f93c60
stacksqueue <paulelyson.villaceran@gmail.com>
2023-11-27 11:12:56 +0800

    prof-ed notes

--- src/app/modules/notes/notes-routing.module.ts
+++ src/app/modules/notes/notes-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotesBiologyCellsComponent } from 'src/app/components/notes-biology-cells/notes-biology-cells.component';
+import { NotesProfessionalEducationComponent } from 'src/app/components/notes-professional-education/notes-professional-education.component';
 import { NotesRizalComponent } from 'src/app/components/notes-rizal/notes-rizal.component';
 import { NotesScienceEarthSpaceAstronomyComponent } from 'src/app/components/notes-science-earth-space-astronomy/notes-science-earth-space-astronomy.component';
 
 const routes: Routes = [
   { path: 'rizal', title: 'LET Rizal Notes', component: NotesRizalComponent },
+  { path: 'professional-education', title: 'Professional Education Notes', component: NotesProfessionalEducationComponent },
   { path: 'science-earth-space-astronomy', title: 'Science - Earth, Space and Astronomy', component: NotesScienceEarthSpaceAstronomyComponent },
   { path: 'science-biology-cells', title: 'Biology - Cells', component: NotesBiologyCellsComponent },
 ];
--- src/app/modules/notes/notes.module.ts
+++ src/app/modules/notes/notes.module.ts
@@ -5,9 +5,10 @@ import { NotesRoutingModule } from './notes-routing.module';
 import { NotesRizalComponent } from '../../components/notes-rizal/notes-rizal.component';
 import { NotesScienceEarthSpaceAstronomyComponent } from 'src/app/components/notes-science-earth-space-astronomy/notes-science-earth-space-astronomy.component';
 import { NotesBiologyCellsComponent } from 'src/app/components/notes-biology-cells/notes-biology-cells.component';
+import { NotesProfessionalEducationComponent } from 'src/app/components/notes-professional-education/notes-professional-education.component';
 
 @NgModule({
-  declarations: [NotesRizalComponent, NotesScienceEarthSpaceAstronomyComponent, NotesBiologyCellsComponent],
+  declarations: [NotesRizalComponent, NotesScienceEarthSpaceAstronomyComponent, NotesBiologyCellsComponent, NotesProfessionalEducationComponent],
   imports: [CommonModule, NotesRoutingModule],
 })
 export class NotesModule {}
