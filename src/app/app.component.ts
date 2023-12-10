5c0c8992cb95dad0e84de5219c7d61467b3caf25
stacksqueue <paulelyson.villaceran@gmail.com>
2023-12-10 14:56:46 +0800

    cookie consent initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgcCookieConsentService, NgcNoCookieLawEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'passThatBoard';
+export class AppComponent implements OnInit, OnDestroy {
+  //keep refs to subscriptions to be able to unsubscribe later
+  private popupOpenSubscription: Subscription;
+  private popupCloseSubscription: Subscription;
+  private initializeSubscription: Subscription;
+  private statusChangeSubscription: Subscription;
+  private revokeChoiceSubscription: Subscription;
+  private noCookieLawSubscription: Subscription;
+
+  constructor(private ccService: NgcCookieConsentService) {}
+
+  ngOnInit() {
+    this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(() => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('pop up open');
+    });
+
+    this.popupCloseSubscription = this.ccService.popupClose$.subscribe(() => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('pop up close');
+    });
+
+    this.initializeSubscription = this.ccService.initialized$.subscribe((event: any) => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('pop up initilized');
+    });
+
+    this.statusChangeSubscription = this.ccService.statusChange$.subscribe((event: NgcStatusChangeEvent) => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('pop up change subscription');
+      this.ccService.fadeIn()
+
+    });
+
+    this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(() => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('pop up revoke subscription');
+
+    });
+
+    this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe((event: NgcNoCookieLawEvent) => {
+      // you can use this.ccService.getConfig() to do stuff...
+      console.log('no cookie law')
+    });
+  }
+
+  ngOnDestroy() {
+    // unsubscribe to cookieconsent observables to prevent memory leaks
+    this.popupOpenSubscription.unsubscribe();
+    this.popupCloseSubscription.unsubscribe();
+    this.initializeSubscription.unsubscribe();
+    this.statusChangeSubscription.unsubscribe();
+    this.revokeChoiceSubscription.unsubscribe();
+    this.noCookieLawSubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,36 @@ import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from './modules/shared/shared.module';
 import { HeaderAdsComponent } from './components/header-ads/header-ads.component';
+import { NgcCookieConsentConfig, NgcCookieConsentModule } from 'ngx-cookieconsent';
+import { environment } from 'src/environments/environment';
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: environment.domain,
+  },
+  position: 'bottom-right',
+  theme: 'edgeless',
+  palette: {
+    popup: {
+      background: '#013832',
+      text: '#ffffff',
+      link: '#ffffff',
+    },
+    button: {
+      background: '#bb35b4',
+      text: '#ffffff',
+      border: 'transparent',
+    },
+  },
+  type: 'opt-out',
+  content: {
+    message: 'This website uses cookies to ensure you get the best experience on our website.',
+    dismiss: 'Got it!',
+    deny: 'Refuse cookies',
+    link: 'Learn more',
+    href: 'https://passthatboard.com/privacy-policy',
+    policy: '😁',
+  },
+};
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HeaderAdsComponent],
@@ -18,7 +48,8 @@ import { HeaderAdsComponent } from './components/header-ads/header-ads.component
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    NgcCookieConsentModule.forRoot(cookieConfig),
   ],
   providers: [],
   bootstrap: [AppComponent],
